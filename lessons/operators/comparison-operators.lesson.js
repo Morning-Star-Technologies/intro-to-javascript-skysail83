3bb95ee9b9a712cc7e15ea821f47fea42ca4ae19
Bryan Nelson <1914742+bryanjnelson@users.noreply.github.com>
2026-04-04 19:38:11 +0000

    Tweaks operators lesson and adds operator challenge

--- lessons/operators/comparison-operators.lesson.js
+++ lessons/operators/comparison-operators.lesson.js
@@ -2,26 +2,26 @@
 let goodSwordsmen = 10;
 let badMagicians = 8;
 
-let result = goodSwordsmen === badMagicians;
+let result = goodSwordsmen === badMagicians; // equality
 
 console.log('Are the good guys and bad guys equal in number?', result);
 
-result = goodSwordsmen !== badMagicians;
+result = goodSwordsmen !== badMagicians; // inequality
 
 console.log('Are the good guys and bad guys different in number?', result);
 
-result = goodSwordsmen > badMagicians;
+result = goodSwordsmen > badMagicians; // greater than
 
 console.log('Are there more good guys than bad guys?', result);
 
-result = goodSwordsmen < badMagicians;
+result = goodSwordsmen < badMagicians; // less than
 
 console.log('Are there fewer good guys than bad guys?', result);
 
-result = goodSwordsmen >= badMagicians;
+result = goodSwordsmen >= badMagicians; // greater than or equal to
 
 console.log('Are there at least as many good guys as bad guys?', result);
 
-result = goodSwordsmen <= badMagicians;
+result = goodSwordsmen <= badMagicians; // less than or equal to
 
 console.log('Are there at least as many bad guys as good guys?', result);
