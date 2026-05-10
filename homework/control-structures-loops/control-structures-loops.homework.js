8d80278ae70c267f9265f1af249f1912e620dd22
Bryan Nelson <1914742+bryanjnelson@users.noreply.github.com>
2026-05-10 19:05:20 +0000

    Updates loops homework on Liam's behalf

--- homework/control-structures-loops/control-structures-loops.homework.js
+++ homework/control-structures-loops/control-structures-loops.homework.js
@@ -6,6 +6,11 @@ const binaryValues = [true, false, true, true, false, false, true, false, false,
 
 let trueCount = 0;
 
-// Replace this comment with your code
+// Count the true values
+for (let i = 0; i < binaryValues.length; i++) {
+    if (binaryValues[i] === true) {
+        trueCount++;
+    }
+}
 
 console.log('The number of true values is', trueCount);
